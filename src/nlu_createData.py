f558f7c912dd94e4e32a521b55cc6368c98b1dc6
simonjisu <simonjisu@gmail.com>
2022-01-06 00:52:54 +0900

    update lower

--- src/nlu_createData.py
+++ src/nlu_createData.py
@@ -371,7 +371,7 @@ class DataCreator:
                         max_length=l
                     )
                     sentences = [
-                        f' {self.f_ENT(target_account)} '.join(aug_sent.split(target_account)).replace('  ', ' ') for aug_sent in sentences
+                        f' {self.f_ENT(target_account)} '.join(aug_sent.split(target_account)).replace('  ', ' ').lower() for aug_sent in sentences
                     ]
                 for s in sentences:
                     entities = []
